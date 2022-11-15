45766e463f75fc318332cdae1b5ca43a9a56a70c
mfritscher555 <mfritscher555@yahoo.com>
2022-11-14 18:53:24 -0600

    load

--- final_project/predictive_models/parser.py
+++ final_project/predictive_models/parser.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-sheet2 = pd.read_excel("./final_project/datasets/dataset.xlsx", sheet_name="temperature_history")
+sheet2 = pd.read_excel("C://Users//Matthew//Documents//GitHub//ISDS-7075-Project//final_project//datasets//dataset.xlsx", sheet_name="temperature_history")
 
 
 """There are 9 different stations where temperature was collected; the excel files shows data from each of the 9 stations
@@ -15,8 +15,11 @@ for each hour of each day. """
 
 
 # Loading data from just station 1 
-station1 = sheet2.loc[sheet2['station_id'] == 1]
+# station1 = sheet2.loc[sheet2['station_id'] == 1]
 
 
+
+# for i in sheet2["h1"]:
+#     print(i)
 
 
